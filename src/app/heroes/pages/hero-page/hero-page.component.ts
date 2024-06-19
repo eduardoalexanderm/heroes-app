d8fd0e4aff3c14ca6d04a5ebd1448f74a34f5623
Eduardo Alexander Ramirez <CLS>
2024-06-18 21:48:12 -0600

    passing a hero by id

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-hero-page',
@@ -6,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroPageComponent implements OnInit {
 
-  constructor() { }
+  public hero?: Hero;
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.pipe(
+      switchMap(({ id }) => this.heroesService.getHeroById(id)),
+    ).subscribe(hero => {
+      if (!hero) return this.router.navigate(['/heroes/list']);
+      this.hero = hero;
+      return;
+    }
+    );
   }
 
 }
