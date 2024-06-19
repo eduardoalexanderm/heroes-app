581e8fced86322feab6ff437b037637a94c89a04
Eduardo Ramirez <eduardoalexanderfcrm@gmail.com>
2024-06-19 15:30:27 -0600

    methods

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -28,4 +28,21 @@ export class HeroesService {
         return this.http_client.get<Hero[]>(`${this.baseUrl}/heroes?q=${term}&_limit=6`);
     }
 
+    addHero(hero: Hero): Observable<Hero> {
+        return this.http_client.post<Hero>(`${this.baseUrl}/heroes`, hero);
+    }
+
+    updateHero(hero: Hero): Observable<Hero> {
+        if (!hero.id) {
+            throw new Error('The hero must have an id');
+        }
+        return this.http_client.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+    }
+
+    deleteHEroById(id: string): Observable<any> {
+        return this.http_client.delete(`${this.baseUrl}/heroes/${id}`);
+    }
+    
+
+
 }
