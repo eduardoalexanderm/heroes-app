79c6e34d9da63efc544aac547f4f70d3ba5235d7
Eduardo Alexander Ramirez <CLS>
2024-06-23 22:17:38 -0600

    guards of auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
   title = 'heroesApp';
 
-  constructor(private authService: AuthService) { }
-  ngOnInit(): void {
-    // this.authService.checkAuthentication().subscribe();
-    // this.authService.checkAuthentication().subscribe(
-    //   () => {
-    //     console.log('Check authentication');
-    //   }
-    // );
-  }
+  
 }
